3123c23ae297c63e7a5138e6c948d89d6a390459
Giovanni Fontana <>
2021-08-06 12:03:38 -0300

    argocd quay

---
+++ quay/deploy-quay-with-argo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Parameters
+# 1. PULL SECRET
+# 
+# Pre-requisites:
+# 1. "oc" cli is already installed
+# 2. "oc" is already logged to the cluster with a cluster-admin user
+# 3. ArgoCD is installed in openshift-gitops namespace
+#
+# Example of Usage:
+# ./deploy-quay-with-argo.sh
+
+
+echo "Deploying Quay using Argo ..."
+oc apply -f deploy-quay.yaml
+
+TIMEOUT=0 
+status=$(oc get quayregistry quay-registry -n openshift-operators -o jsonpath='{.status.conditions[0]}')
+while [ "$status" != "Available" ]; do
+  echo "It still being deployed. Waiting one more minute..."
+  sleep 60
+  if [ $TIMEOUT -gt 15 ]; then #15 MINUTES TIMEOUT
+    echo "Timeout reached... Check the status of ACM deployment on OpenShift."
+    exit 1
+  fi
+  TIMEOUT=$(($TIMEOUT+1))
+  status=$(oc get quayregistry quay-registry -n openshift-operators -o jsonpath='{.status.conditions[0]}')
+done
+
+echo "Deployment Finished"
