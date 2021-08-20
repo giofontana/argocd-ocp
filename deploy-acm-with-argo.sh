8a28c40d1d9b4a3cfda2ec87311dce372a034446
Giovanni Fontana <>
2021-08-20 19:15:32 -0300

    quay

--- deploy-acm-with-argo.sh
+++ deploy-acm-with-argo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Parameters
-# 1. PULL SECRET
 # 
 # Pre-requisites:
 # 1. "oc" cli is already installed
@@ -9,33 +7,8 @@
 # 3. ArgoCD is installed in openshift-gitops namespace
 #
 # Example of Usage:
-# ./deploy-acm-with-argo.sh '{"auths":{"cloud.openshift.com":{"auth":"b3B...}}}'
+# ./deploy-acm-with-argo.sh
 
 
-echo "Creating ACM Namespace ..."
-oc create namespace openshift-advanced-cluster-mgmt
-
-echo "Creating ACM Pull Secret ..."
-echo "$1" > pull-secret.txt
-oc create secret generic acm-pullsecret -n openshift-advanced-cluster-mgmt --from-file=.dockerconfigjson=pull-secret.txt --type=kubernetes.io/dockerconfigjson
-rm -f pull-secret.txt
-
-echo "Deploying ACM ..."
-oc apply -f deploy-operator.yaml
-oc apply -f deploy-acm.yaml
-
-sleep 60
-TIMEOUT=0 
-mch_status=$(oc get MultiClusterHub multiclusterhub -n openshift-advanced-cluster-mgmt -o jsonpath='{.status.phase}')
-while [ "$mch_status" != "Running" ]; do
-  echo "MultiClusterHub still being deployed. Waiting one more minute..."
-  sleep 60
-  if [ $TIMEOUT -gt 30 ]; then #30 MINUTES TIMEOUT
-    echo "Timeout reached... Check the status of the deployment on OpenShift."
-    exit 1
-  fi
-  TIMEOUT=$(($TIMEOUT+1))
-  mch_status=$(oc get MultiClusterHub multiclusterhub -n openshift-advanced-cluster-mgmt -o jsonpath='{.status.phase}')
-done
-
-echo "ACM Deployment Finished"
+echo "Deploying ACM using ArgoCD..."
+./argo-app-runner.sh acm 'https://github.com/giofontana/argocd-ocp.git' 'acm/gitops' HEAD
