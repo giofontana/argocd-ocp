c37d2f07ccd05e0530e89e3ed0a3f5590a3e2115
Giovanni Fontana <>
2021-08-20 12:58:09 -0300

    ACS fix

--- acs/deploy-acs-with-argo.sh
+++ acs/deploy-acs-with-argo.sh
@@ -9,23 +9,5 @@
 # Example of Usage:
 # ./deploy-acs-with-argo.sh
 
-echo "Deploying ACS ..."
+echo "Deploying ACS using ArgoCD..."
 oc apply -f deploy-acs.yaml
-
-sleep 60
-TIMEOUT=0 
-status=$(oc get Central stackrox-central-services -n stackrox -o jsonpath='{.status.conditions[0].type}')
-while [ "$status" != "Deployed" ]; do
-  echo "It still being deployed. Waiting one more minute..."
-  sleep 60
-  if [ $TIMEOUT -gt 15 ]; then #15 MINUTES TIMEOUT
-    echo "Timeout reached... Check the status of the deployment on OpenShift."
-    exit 1
-  fi
-  TIMEOUT=$(($TIMEOUT+1))
-  status=$(oc get Central stackrox-central-services -n stackrox -o jsonpath='{.status.conditions[0].type}')
-done
-
-central_password=$(oc get Central stackrox-central-services -n stackrox -o jsonpath='{.status.central.adminPassword.info}')
-
-echo "Deployment Finished! $central_password"
